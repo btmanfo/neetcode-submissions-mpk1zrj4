892447c7a0387244aadef939d500cd046c1c1aa4
Tangmouo Manfo, Brice <144279774+btmanfo@users.noreply.github.com>
2026-05-19 09:30:42 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervalTable = []
+        for i in intervals:
+            intervalTable.append([i.start, i.end])
+        if len(intervalTable) == 0:
+            return True
+        heapq.heapify(intervalTable)
+        start, end = heapq.heappop(intervalTable)
+        print(intervalTable)
+        while len(intervalTable) > 0:
+            newStart, newEnd = heapq.heappop(intervalTable)
+            if end > newStart:
+                return False
+        # for i in range(1, len(intervalTable)):
+        #     if intervalTable[i-1][1] > intervalTable[i][0]:
+        #         return False
+        return True
